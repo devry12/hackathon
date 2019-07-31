03ff4b9819f0c13c8f602b01252f3a3b450ce234
David Rigan <david.rigan@students.amikom.ac.id>
2019-07-31 11:09:56 +0700

    add template

---
+++ application/views/template/header.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title><?= $title_template; ?></title>
+  <link rel="stylesheet" href="./assets/bootstrap/css/bootstrap.css">
+  <link rel="stylesheet" href="./assets/smartedu/css/smartedu.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.9.0/css/all.css">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fullcalendar/core@4.2.0/main.min.js">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fullcalendar/core@4.2.0/main.min.css">
+
+  <!-- Font -->
+  <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Open+Sans" />
+</head>
